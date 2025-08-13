ed76f96f4ef25ad010df20322520c00d1e86c5b5
Totox00 <bladewielder.roboman@gmail.com>
2025-08-14 01:20:39 +0200

    updated valid games list and resolve old names of PokePark

--- clean_yamls/src/game.rs
+++ clean_yamls/src/game.rs
@@ -1,3 +1,4 @@
+use hashlink::LinkedHashMap;
 use rand::thread_rng;
 use rand_distr::{Distribution, WeightedIndex};
 use yaml_rust2::Yaml;
@@ -6,6 +7,11 @@ use crate::valid_games::VALID_GAMES;
 
 pub fn choose_game(doc: &mut Yaml) -> Option<Yaml> {
     let game_key = Yaml::from_str("game");
+
+    if let Some(hash) = doc.as_mut_hash() {
+        rename_game(hash, "PokéPark Wii: Pikachu's Adventure [JP]", "PokePark");
+        rename_game(hash, "PokéPark", "PokePark");
+    }
 
     if let Some(games) = doc.as_mut_hash()?.get_mut(&game_key) {
         let game = match games {
@@ -21,6 +27,12 @@ pub fn choose_game(doc: &mut Yaml) -> Option<Yaml> {
                     })
                     .collect();
 
+                for (game, _) in &mut games {
+                    if *game == "PokéPark Wii: Pikachu's Adventure [JP]" || *game == "PokéPark" {
+                        *game = "PokePark";
+                    }
+                }
+
                 if games.iter().any(|(game, weight)| *weight > 0 && VALID_GAMES.contains(game)) {
                     games.retain(|(game, _)| VALID_GAMES.contains(game));
                 }
@@ -28,7 +40,12 @@ pub fn choose_game(doc: &mut Yaml) -> Option<Yaml> {
                 let dist = WeightedIndex::new(games.iter().map(|(_, weight)| weight)).expect("Failed to create index");
                 Yaml::from_str(games[dist.sample(&mut rng)].0)
             }
-            Yaml::String(game) => Yaml::from_str(game),
+            Yaml::String(game) => {
+                if *game == "PokéPark Wii: Pikachu's Adventure [JP]" || *game == "PokéPark" {
+                    *game = String::from("PokePark");
+                }
+                Yaml::from_str(game)
+            }
             _ => return None,
         };
 
@@ -36,5 +53,11 @@ pub fn choose_game(doc: &mut Yaml) -> Option<Yaml> {
         Some(game)
     } else {
         None
+    }
+}
+
+fn rename_game(hash: &mut LinkedHashMap<Yaml, Yaml>, from: &str, to: &str) {
+    if let Some(options) = hash.remove(&Yaml::from_str(from)) {
+        hash.insert(Yaml::from_str(to), options);
     }
 }
