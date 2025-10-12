3c62ae14a09ec5efd2ed9df3d12c55461f6d12ec
Totox00 <bladewielder.roboman@gmail.com>
2025-10-12 04:46:35 +0200

    added note for logic for phantom hourglass

--- common/src/special.rs
+++ common/src/special.rs
@@ -678,6 +678,7 @@ pub fn handle_special(doc: &mut Yaml, game: &Yaml, name: &str) -> Vec<String> {
                 game_hash.insert(Yaml::from_str("disable_author_locations"), disable_author);
             }
         }
+        Some("The Legend of Zelda - Phantom Hourglass") => push_value_or_default(&mut notes, game_hash, "logic", "normal"),
         _ => (),
     };
 
